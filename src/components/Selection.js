777fc350123ad0f165e13d326a72be65f19df90b
Shaik-Mohammed-Zubaidi <68868510+Shaik-Mohammed-Zubaidi@users.noreply.github.com>
2020-11-29 01:37:47 +0530

    c11

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,19 @@
+import React, { useState } from "react";
+import "../styles/Child.css";
 
+const Selection = (props) => {
+  const { applyColor } = props;
+
+  const [styleVar, setStyleVar] = useState({ background: "" });
+
+  const handleClick = () => {
+    applyColor(setStyleVar);
+  };
+  return (
+    <div className="fix-box" style={styleVar} onClick={handleClick}>
+      <h2 className="subheading">Selection</h2>
+    </div>
+  );
+};
+
+export default Selection;
